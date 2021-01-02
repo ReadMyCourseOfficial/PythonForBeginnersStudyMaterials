962efb844334fdfe2c5d54e64c70faaac6fed774
anand84471 <35921176+anand84471@users.noreply.github.com>
2021-01-02 21:07:19 +0530

    initial commit

---
+++ codes/Day2/comment_example.py
@@ -0,0 +1,10 @@
+#single line comment
+#single line comment 2
+#single line comment 3
+print("Hello world")
+"""
+This is comment line 1
+this is comment line 2
+this is comment line 3
+"""
+
---
+++ codes/Day2/print_function_example.py
@@ -0,0 +1,6 @@
+# print("Message",end="**")
+# print("This is my message 2")
+# print("This is my fucntion")
+print("This is message 1","This is message 2","This is message 3",sep="*")
+
+
